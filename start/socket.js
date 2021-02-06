7627970fbeff5cd6e9e33b81ece4d996afc62e5f
Tomi <developer@livestudio.eu>
2021-02-06 16:14:18 +0100

    fix socket io events

--- start/socket.js
+++ start/socket.js
@@ -1,7 +1,11 @@
 const Server = use("Server");
 const io = use("socket.io")(Server.getInstance(), {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3006", "http://tomise20.hu/"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3006",
+      "http://food.tomise20.hu",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -12,35 +16,21 @@ let sockets = [];
 io.on("connection", function (socket) {
   sockets[socket.id] = socket;
 
-  socket.on("user join", (roomId) => {
-    socket.join(roomId);
-    console.log("user joinned to room!", roomId);
-    io.to(roomId).emit("join accepted", "accepted");
-  });
-
   socket.on("admin join", () => {
     socket.join("admins");
     console.log("admin joinned to wait room!");
     io.to("admins").emit("success joined", "success");
   });
 
-  socket.on("admin join room", (roomId) => {
-    console.log("admin joinned to private room!", roomId);
-    socket.join(roomId);
+  socket.on("admin accepted", (socketId) => {
+    io.to(socketId).emit("admin joined", socket.id);
   });
 
-  socket.on("open new chat", (roomId, message) => {
-    console.log("admin get a chat request", roomId);
-    io.to("admins").emit("user join request", roomId, message);
+  socket.on("open new chat", (message) => {
+    io.to("admins").emit("user join request", socket.id, message);
   });
 
-  socket.on("admin join private room", (roomId) => {
-    console.log("admin joinned to private room!", roomId);
-    socket.join(roomId);
-  });
-
-  socket.on("private message", (roomId, msg) => {
-    console.log("send a private message!", roomId);
-    socket.to(roomId).emit(msg);
+  socket.on("private message", (anotherSocketId, msg) => {
+    socket.to(anotherSocketId).emit("private message", socket.id, msg);
   });
 });
